7c465aac009d42f1bdfcf36e5fca3124fdab01b5
Devin <liudev777@gmail.com>
2022-06-21 20:54:39 -0500

    update

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,9 @@
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="app">
         
-      </header>
+      </div>
     </div>
   );
 }
---
+++ portfolio/src/components/greeting/greeting.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function greeting() {
+  return (
+    <div className='greeting'>
+      
+    </div>
+  )
+}
